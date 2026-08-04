2cd448aa316368bdfcafb59dc44a07812746df0e
F.A.M-TECH <108268598+famtec@users.noreply.github.com>
2026-08-04 21:06:50 +0300

    تثبيت منفذ الإقلاع الديناميكي للسيرفر السحابي

--- main.py
+++ main.py
@@ -850,4 +850,5 @@ app.mount("/", StaticFiles(directory="frontend_web", html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    port = int(os.environ.get("PORT", 8090))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
